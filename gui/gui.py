1ec419998656bd176d4f8d080044bdc25407e61d
Xuan Song <songxuan319@gmail.com>
2015-08-03 14:14:12 +0200

    gui.py updated

--- gui/gui.py
+++ gui/gui.py
@@ -8,96 +8,44 @@ class ourwindow(Gtk.Window):
     def __init__(self):
         # Init the window
         Gtk.Window.__init__(self, title="Client Certificate Generator")
-        self.set_default_size(500,300 )
         self.set_border_width(10)
 
         # Init a gtk box
-        hbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL,spacing=50)
-        self.add(hbox)
+        self.hbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL,spacing=0)
+        self.add(self.hbox)
 
+    def createFields(self, name):
         # Init a listbox
         inputbox = Gtk.ListBox()
         inputbox.set_selection_mode(Gtk.SelectionMode.NONE)
-        hbox.pack_start(inputbox, True, True, 0)
+        self.hbox.pack_start(inputbox, True, True, 0)
 
         # Init a row 
         row = Gtk.ListBoxRow()
-
+  
         # Init a horizontal box
-        hbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
+        hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=50)
         row.add(hbox)
         # Pack a vertical box into hbox
-        vbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=10)
-        hbox.pack_start(vbox, True, True, 0)
+        vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=50)
+        self.hbox.pack_start(vbox, True, True, 0)
         # Pack a label and an entry into vbox
-        label = Gtk.Label("Domain Name", width_chars = 15 )
-        entry = Gtk.Entry(xalign = 1)
-        vbox.pack_start(label, True, True, 0)
-        vbox.pack_start(entry, True, True, 0)
-
-        # Add the row to the list box
-        inputbox.add(row)
-
-        
-        # Init a row 
-        row = Gtk.ListBoxRow()
-
-        # Init a horizontal box
-        hbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
-        row.add(hbox)
-        # Pack a vertical box into hbox
-        vbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=10)
-        hbox.pack_start(vbox, True, True, 0)
-        # Pack a label and an entry into vbox
-        label = Gtk.Label("Common Name", width_chars = 15 )
-        entry = Gtk.Entry(xalign = 1)
-        vbox.pack_start(label, True, True, 0)
-        vbox.pack_start(entry, True, True, 0)
-
-        # Add the row to the list box
-        inputbox.add(row)
-
-
-
-        # Init a row 
-        row = Gtk.ListBoxRow()
-
-        # Init a horizontal box
-        hbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
-        row.add(hbox)
-        # Pack a vertical box into hbox
-        vbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=10)
-        hbox.pack_start(vbox, True, True, 0)
-        # Pack a label and an entry into vbox
-        label = Gtk.Label("Country Name", width_chars = 15 )
-        entry = Gtk.Entry(xalign = 1)
-        vbox.pack_start(label, True, True, 0)
-        vbox.pack_start(entry, True, True, 0)
-
-        # Add the row to the list box
-        inputbox.add(row)
-
-        
-        # Init a row 
-        row = Gtk.ListBoxRow()
-
-        # Init a horizontal box
-        hbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
-        row.add(hbox)
-        # Pack a vertical box into hbox
-        vbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=10)
-        hbox.pack_start(vbox, True, True, 0)
-        # Pack a label and an entry into vbox
-        label = Gtk.Label("Email", width_chars = 15 )
+        label = Gtk.Label(name, width_chars = 15 )
         entry = Gtk.Entry()
-        vbox.pack_start(label, True, True, 0)
-        vbox.pack_start(entry, True, True, 0)
+        hbox.pack_start(label, True, True, 0)
+        hbox.pack_start(entry, True, True, 0)
 
         # Add the row to the list box
         inputbox.add(row)
+            
 
-
-window = ourwindow()        
-window.connect("delete-event", Gtk.main_quit)
-window.show_all()
-Gtk.main()
+if __name__ == "__main__":
+    
+    window = ourwindow()
+    window.createFields("Domain Name")
+    window.createFields("Common Name")
+    window.createFields("Country Name")
+    window.createFields("Email")
+    window.connect("delete-event", Gtk.main_quit)
+    window.show_all()
+    Gtk.main()
