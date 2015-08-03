7e55ab49e3f52ab928be30859465389af92dad3d
Xuan Song <songxuan319@gmail.com>
2015-08-03 15:29:59 +0200

    All else but making the cert needs to be implemented

--- gui/gui.py
+++ gui/gui.py
@@ -15,37 +15,52 @@ class ourwindow(Gtk.Window):
         self.add(self.hbox)
 
     def createFields(self, name):
-        # Init a listbox
-        inputbox = Gtk.ListBox()
-        inputbox.set_selection_mode(Gtk.SelectionMode.NONE)
-        self.hbox.pack_start(inputbox, True, True, 0)
-
-        # Init a row 
-        row = Gtk.ListBoxRow()
   
-        # Init a horizontal box
-        hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=50)
-        row.add(hbox)
         # Pack a vertical box into hbox
         vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=50)
         self.hbox.pack_start(vbox, True, True, 0)
+
+        # Init a horizontal box
+        hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=50)
+        vbox.pack_start(hbox, True, True, 0)
+
         # Pack a label and an entry into vbox
         label = Gtk.Label(name, width_chars = 15 )
-        entry = Gtk.Entry()
+        self.entry = Gtk.Entry()
         hbox.pack_start(label, True, True, 0)
-        hbox.pack_start(entry, True, True, 0)
+        hbox.pack_start(self.entry, True, True, 0)
 
-        # Add the row to the list box
-        inputbox.add(row)
-            
+        return(self.entry)
+    
+    def addButton(self):
+        button = Gtk.Button("Create")
+        button.connect("clicked", self.on_click_button_clicked)
+        self.hbox.pack_start(button, True, True, 0)
+        
+    def on_click_button_clicked(self,button):
+       for i in self.info:
+           if(i.get_text()):     # Empty check
+               print i.get_text()
+           else:
+               print "Empty value!"
 
+    def collectInfo(self, dn, comn, coun, e, val):
+        self.info = [dn, comn, coun, e, val]
+        
+        
 if __name__ == "__main__":
     
     window = ourwindow()
-    window.createFields("Domain Name")
-    window.createFields("Common Name")
-    window.createFields("Country Name")
-    window.createFields("Email")
+
+    dn = window.createFields("Domain")
+    comn = window.createFields("Common Name")
+    coun = window.createFields("Country")
+    e = window.createFields("Email")
+    val = window.createFields("Validity")
+
+    window.addButton()
+    window.collectInfo(dn, comn, coun, e, val)
+    
     window.connect("delete-event", Gtk.main_quit)
     window.show_all()
     Gtk.main()
