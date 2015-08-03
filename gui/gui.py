b4b769b067501278260da570082dfa326148564a
Xuan Song <songxuan319@gmail.com>
2015-08-03 13:51:35 +0200

    gui init designed

--- gui/gui.py
+++ gui/gui.py
@@ -6,17 +6,96 @@ from gi.repository import Gtk
 class ourwindow(Gtk.Window):
 
     def __init__(self):
-        Gtk.Window.__init__(self, title="My Hello World Program")
-        Gtk.Window.set_default_size(self, 400,325)
-        Gtk.Window.set_position(self, Gtk.WindowPosition.CENTER)
+        # Init the window
+        Gtk.Window.__init__(self, title="Client Certificate Generator")
+        self.set_default_size(500,300 )
+        self.set_border_width(10)
 
-        button1 = Gtk.Button("Hello, World!")
-        button1.connect("clicked", self.whenbutton1_clicked)
+        # Init a gtk box
+        hbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL,spacing=50)
+        self.add(hbox)
 
-        self.add(button1)
+        # Init a listbox
+        inputbox = Gtk.ListBox()
+        inputbox.set_selection_mode(Gtk.SelectionMode.NONE)
+        hbox.pack_start(inputbox, True, True, 0)
+
+        # Init a row 
+        row = Gtk.ListBoxRow()
+
+        # Init a horizontal box
+        hbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
+        row.add(hbox)
+        # Pack a vertical box into hbox
+        vbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=10)
+        hbox.pack_start(vbox, True, True, 0)
+        # Pack a label and an entry into vbox
+        label = Gtk.Label("Domain Name", width_chars = 15 )
+        entry = Gtk.Entry(xalign = 1)
+        vbox.pack_start(label, True, True, 0)
+        vbox.pack_start(entry, True, True, 0)
+
+        # Add the row to the list box
+        inputbox.add(row)
+
         
-    def whenbutton1_clicked(self, button):
-      print "Hello, World!"
+        # Init a row 
+        row = Gtk.ListBoxRow()
+
+        # Init a horizontal box
+        hbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
+        row.add(hbox)
+        # Pack a vertical box into hbox
+        vbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=10)
+        hbox.pack_start(vbox, True, True, 0)
+        # Pack a label and an entry into vbox
+        label = Gtk.Label("Common Name", width_chars = 15 )
+        entry = Gtk.Entry(xalign = 1)
+        vbox.pack_start(label, True, True, 0)
+        vbox.pack_start(entry, True, True, 0)
+
+        # Add the row to the list box
+        inputbox.add(row)
+
+
+
+        # Init a row 
+        row = Gtk.ListBoxRow()
+
+        # Init a horizontal box
+        hbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
+        row.add(hbox)
+        # Pack a vertical box into hbox
+        vbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=10)
+        hbox.pack_start(vbox, True, True, 0)
+        # Pack a label and an entry into vbox
+        label = Gtk.Label("Country Name", width_chars = 15 )
+        entry = Gtk.Entry(xalign = 1)
+        vbox.pack_start(label, True, True, 0)
+        vbox.pack_start(entry, True, True, 0)
+
+        # Add the row to the list box
+        inputbox.add(row)
+
+        
+        # Init a row 
+        row = Gtk.ListBoxRow()
+
+        # Init a horizontal box
+        hbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
+        row.add(hbox)
+        # Pack a vertical box into hbox
+        vbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=10)
+        hbox.pack_start(vbox, True, True, 0)
+        # Pack a label and an entry into vbox
+        label = Gtk.Label("Email", width_chars = 15 )
+        entry = Gtk.Entry()
+        vbox.pack_start(label, True, True, 0)
+        vbox.pack_start(entry, True, True, 0)
+
+        # Add the row to the list box
+        inputbox.add(row)
+
 
 window = ourwindow()        
 window.connect("delete-event", Gtk.main_quit)
