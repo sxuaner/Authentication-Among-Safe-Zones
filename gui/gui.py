27cddd86677ddb3f4fb8f779c60c2e642aeaf78f
Xuan Song <songxuan319@gmail.com>
2015-08-17 12:32:21 +0200

    on_click_show_clicked. on_click_fivereqbutton_clicked, on_click_reqbutton_clicked, updateEntries modified

--- gui/gui.py
+++ gui/gui.py
@@ -19,20 +19,28 @@ class ourwindow(Gtk.Window):
         self.entries = []
         # dict for storing field names
         self.info = dict()
-        # list for storing names
+        # list for storing names of users
         self.names = []
+
+    def layout(self):
+            self.createFields("Alias")
+            self.createFields("Common Name")
+            self.createFields("Store Pass")
+            self.createFields("Key Size")
+            self.createFields("Validity")
+            self.createFields("Domain")
+            self.createFields("Country Code")
+            self.createFields("Email")
+            self.createFields("Key Pass")
+            self.createFields("CA's Path")
 
     def createFields(self, name):
 
-        # Pack a vertical box into hbox
-        vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=50)
-        self.box.pack_start(vbox, True, True, 0)
-
         # Init a horizontal box
         hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=50)
-        vbox.pack_start(hbox, True, True, 0)
+        self.box.pack_start(hbox, True, True, 0)
 
-        # Pack a label and an entry into vbox
+        # Pack a label and an entry
         label = Gtk.Label(name, width_chars = 15 )
         entry = Gtk.Entry()
         hbox.pack_start(label, True, True, 0)
@@ -43,7 +51,6 @@ class ourwindow(Gtk.Window):
         self.names.append(name)
         self.info[name] = entry.get_text()
         
-        # button part
     def createButton(self):
         # A warning when some fields are empty should be made.
         vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL,spacing= 1)
@@ -57,11 +64,6 @@ class ourwindow(Gtk.Window):
         button.connect("clicked", self.on_click_keystore_clicked)
         hbox.pack_start(button, True, True, 0)
 
-        button = Gtk.Button("Request")
-        button.connect("clicked", self.on_click_reqbutton_clicked)
-        hbox.pack_start(button, True, True, 0)
-
-        # Hbox for show cert button
         hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL,spacing= 1)
         vbox.pack_start(hbox, True, True, 0)
 
@@ -69,24 +71,41 @@ class ourwindow(Gtk.Window):
         button.connect("clicked", self.on_click_show_clicked)
         hbox.pack_start(button, True, True, 0)
 
+        # Hbox for show cert button
         hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL,spacing= 1)
         vbox.pack_start(hbox, True, True, 0)
 
         button = Gtk.Button("Select Ca")
         button.connect("clicked", self.on_file_clicked)
         hbox.pack_start(button, True, True, 0)
-        
-        # To update and display the input values:
-    def updateEntries(self):
 
+        # To update and display the input values:
+
+        # Hbox for show cert button
+        hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL,spacing= 1)
+        vbox.pack_start(hbox, True, True, 0)
+        
+        button = Gtk.Button("A Request")
+        button.connect("clicked", self.on_click_reqbutton_clicked)
+        hbox.pack_start(button, True, True, 0)
+
+        button = Gtk.Button("Five Requests")
+        button.connect("clicked", self.on_click_fivereqbutton_clicked)
+        hbox.pack_start(button, True, True, 0)
+        
+
+
+    def updateEntries(self):
         print "\n\n ------------ "
+
         for entry, name in zip(self.entries, self.names):
+
             tmp = entry.get_text()
+            self.info[name] = tmp
             if tmp:
-                self.info[name] = tmp
-                print tmp
+                print '{0:20s} {1:20}'.format(name, tmp)
             else:
-                print name, " Empty value "
+                print '{0:20} {1:20}'.format(name, "Empty Value")
         print " ------------ \n\n "
 
     def on_click_keystore_clicked(self, button):
@@ -112,36 +131,80 @@ class ourwindow(Gtk.Window):
                            "-alias", str( self.info["Alias"]),
                            "-dname", str("CN=" + self.info["Common Name"]),
                            "-keystore", "../keystore",
-                           "-keysize", str(self.info["Key Size"])
+                           "-keysize", str(self.info["Key Size"]),
+                           "-keypass", str(self.info["Key Pass"]),
+                           "-storepass", str(self.info["Store Pass"])
                     ]
                     subprocess.Popen(par)
                 else:
                     print "Alias is not provided"
             else:
                 print "Common Name is not provided"
+        else:
+            print "Your keystore has already been created! You can find it in the top directory."
 
     def on_click_reqbutton_clicked(self, button):
         self.updateEntries()
 
+        # Make sure folder ./clientCerts exist.
+        if not os.path.exists("../clientCerts"):
+            par = ["mkdir",
+                   "-p", "../clientCerts"
+            ]
+            subprocess.Popen(par)
+
+        # @par:
+        #  -certreq: make a cert request
+        #  -alias: alias name used in key store
+        #  -file: output file
+        #  -keystore Path to keystore
+        #  -storepass , self-explained
+
         par = ["/bin/keytool",
                "-certreq",
-               "-alias",
-               str( self.info["Alias"]),
-               "-file", str( self.info["Alias"]+".pem")
+               "-alias", str( self.info["Alias"]),
+               "-file", str( "../clientCerts/"+ self.info["Common Name"]+".pem"),
+               "-keystore", "../keystore",
+               "-storepass", str(self.info["Store Pass"])
         ]
         subprocess.Popen(par)
+
+    def on_click_fivereqbutton_clicked(self, button):
+        self.updateEntries()
+
+        # To make sure the dir exists
+        if not os.path.exists("../clientCerts"):
+            par = ["mkdir",
+                   "-p", "../clientCerts"
+            ]
+            subprocess.Popen(par)
+
+        # Make 5 certs based on common name
+        for i in range(0,5):
+            par = ["/bin/keytool",
+                   "-certreq",
+                   "-alias", str( self.info["Alias"]),
+                   "-file", str( "../clientCerts/" + self.info["Common Name"]+ str(i) + ".pem"),
+                   "-keystore", "../keystore",
+                   "-storepass", str(self.info["Store Pass"])
+            ]
+            subprocess.Popen(par)
+
+    
 
     def on_click_show_clicked(self,button):
         self.updateEntries()
 
+        # @par -list: argument to list the content of a keystore
+        # @par -keystore: where to find target keystore, followed by the path.
         par = ["/bin/keytool",
                "-list",
-               "-keystore",
-               "../keystore"
+               "-keystore", "../keystore",
+               "-storepass", str(self.info["Store Pass"])
         ]
         subprocess.Popen(par)
 
-
+##################### This part belongs to file selection window ######################
     def on_file_clicked(self, widget):
         dialog = Gtk.FileChooserDialog("Please choose a file", self,
                                        Gtk.FileChooserAction.OPEN,
@@ -187,24 +250,13 @@ class ourwindow(Gtk.Window):
             print("Folder selected: " + dialog.get_filename())
         elif response == Gtk.ResponseType.CANCEL:
             print("Cancel clicked")
-
         dialog.destroy()
-        
+###################### End of file selection window ######################        
     
 if __name__ == "__main__":
     window = ourwindow()
-
-    window.createFields("Alias")
-    window.createFields("Common Name")
-    window.createFields("Key Size")
-    window.createFields("Validity")
-    window.createFields("Domain")
-    window.createFields("Country Code")
-    window.createFields("Email")
-    
-
+    window.layout()
     window.createButton()
-
     window.connect("delete-event", Gtk.main_quit)
     window.show_all()
     Gtk.main()
